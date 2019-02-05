a4d7cdbc1bed2d3bd9e1eb2efde00ff3b923a303
pablospl <43990474+pablospl@users.noreply.github.com>
2019-02-05 09:46:53 +0100

    Update 2019_01_31_191313_orderss.php

--- Migraciones y Dataseeder/2019_01_31_191313_orderss.php
+++ Migraciones y Dataseeder/2019_01_31_191313_orderss.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Orderss extends Migration
+class Orders extends Migration
 {
 	protected $primaryKey='id_order';
     /**
